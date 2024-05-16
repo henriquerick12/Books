61c6e67b305976327dd06f096f4c314d0efc8af3
henriquerick12 <henriquewood3@gmail.com>
2024-05-15 21:58:06 -0300

    Feat: react-router-dom routes

--- src/views/AddBookPage.jsx
+++ src/views/AddBookPage.jsx
@@ -1,45 +1,52 @@
-import Header from '../components/Header.jsx';
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header.jsx";
 
 function AddBookPage() {
-    
-    const pageTitle = "Add Book";
+  const navigate = useNavigate();
 
-    return (
-      <>
-        <div className="container">
-            <Header pageTitle={pageTitle} />
+  const pageTitle = "Add Book";
 
-            <form className="add-form">
-                <div className="form-control">
-                    <label>Title *</label>
-                    <input type="text" name="title" placeholder="Add Book Title" />
-                </div>
-                <div className="form-control">
-                    <label>Book Cover *</label>
-                    <input type="text" name="cover" placeholder="Add Cover" />
-                </div>
+  const handleSubmitAdd = (e) => {
+    e.preventDefault();
+    alert("ok");
 
-                <div className="form-control">
-                <label>Author *</label>
-                <input
-                    type="text" name="author" placeholder="Add Author" />
-                </div>
+    navigate("/");
+  };
 
-                <div className="form-control">
-                <label>Synopsis *</label>
-                <textarea
-                    type="text" name="synopsis" placeholder="Add a synopsis..." />
-                </div>
-                
-                <button className="btn btn-block">Save Book</button>
-            </form>
+  return (
+    <>
+      <div className="container">
+        <Header pageTitle={pageTitle} />
 
-        </div>
+        <form className="add-form" onSubmit={(e) => handleSubmitAdd(e)}>
+          <div className="form-control">
+            <label>Title *</label>
+            <input type="text" name="title" placeholder="Add Book Title" />
+          </div>
+          <div className="form-control">
+            <label>Book Cover *</label>
+            <input type="text" name="cover" placeholder="Add Cover" />
+          </div>
 
-        
-      </>
-    )
-  }
-  
-  export default AddBookPage
-  
+          <div className="form-control">
+            <label>Author *</label>
+            <input type="text" name="author" placeholder="Add Author" />
+          </div>
+
+          <div className="form-control">
+            <label>Synopsis *</label>
+            <textarea
+              type="text"
+              name="synopsis"
+              placeholder="Add a synopsis..."
+            />
+          </div>
+
+          <button className="btn btn-block">Save Book</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default AddBookPage;
